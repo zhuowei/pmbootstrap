b2bfe2cfca5df56a293ecfa0c55c548efda5b17b
Clayton Craft <clayton@craftyguy.net>
2017-08-20 14:21:58 -0700

    lower default iter-time even further

--- pmb/config/__init__.py
+++ pmb/config/__init__.py
@@ -63,7 +63,7 @@ defaults = {
     "cipher": "aes-cbc-plain64",
     # A higher value is typically desired, but this can lead to VERY long open
     # times on slower devices
-    "iter_time": "500",
+    "iter_time": "200",
     # Hash algorithm to use, note that SHA1 is considered fine even though default
     # for cryptsetup is sha256:
     # https://www.kernel.org/pub/linux/utils/cryptsetup/v1.7/v1.7.0-ReleaseNotes
